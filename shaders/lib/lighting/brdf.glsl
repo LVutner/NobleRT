01ced8e0529ec3b06aab50ccdcc999ebbd3bd7c6
LVutner <kkamil724@gmail.com>
2021-09-21 01:38:52 +0200

    * UPD: [Lighting] Changed fresnel term for DFV function

--- shaders/lib/lighting/brdf.glsl
+++ shaders/lib/lighting/brdf.glsl
@@ -114,7 +114,7 @@ vec3 envBRDFApprox(vec3 F0, float NdotV, float roughness) {
     const vec4 c0 = vec4(-1.0, -0.0275, -0.572, 0.022);
     const vec4 c1 = vec4(1.0, 0.0425, 1.04, -0.04);
     vec4 r = roughness * c0 + c1;
-    float a004 = min(r.x * r.x, exp2(-9.28 * NdotV)) * r.x + r.y;
+    float a004 = min(r.x * r.x, exp2(((-5.55473 * NdotV) - 6.98316) * NdotV)) * r.x + r.y;
     vec2 AB = vec2(-1.04, 1.04) * a004 + r.zw;
     return F0 * AB.x + AB.y;
 }
